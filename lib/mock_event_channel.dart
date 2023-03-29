78cd54685cac34c25164174620df6ec0c150c19b
Rexios <rexios@rexios.dev>
2023-03-29 15:28:58 -0400

    Working on it

--- lib/mock_event_channel.dart
+++ lib/mock_event_channel.dart
@@ -1,7 +1,79 @@
-library mock_event_channel;
+import 'dart:async';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+/// Extension on [EventChannel] to allow mocking of the stream
+extension TestEventChannelExtension on EventChannel {
+  /// Set a mock stream handler for this channel
+  void setMockStreamHandler(MockStreamHandler handler) {
+    final controller = StreamController<Object>();
+
+    MethodChannel(name, codec).setMockMethodCallHandler((call) async {
+      switch (call.method) {
+        case 'listen':
+          return handler.onListen(
+            call.arguments,
+            MockStreamHandlerEventSink(controller.sink),
+          );
+        case 'cancel':
+          return handler.onCancel(call.arguments);
+        default:
+          throw UnimplementedError('Method ${call.method} not implemented');
+      }
+    });
+
+    final sub = controller.stream.listen(
+      (e) => binaryMessenger.send(name, codec.encodeSuccessEnvelope(e)),
+    );
+    sub.onError((e) {
+      if (e is! PlatformException) {
+        throw e;
+      }
+      binaryMessenger.send(
+        name,
+        codec.encodeErrorEnvelope(
+          code: e.code,
+          message: e.message,
+          details: e.details,
+        ),
+      );
+    });
+    sub.onDone(
+      () => binaryMessenger.send(name, codec.encodeSuccessEnvelope(null)),
+    );
+  }
+}
+
+/// A mock stream handler for an [EventChannel]
+abstract class MockStreamHandler {
+  /// Handler for the listen event
+  void onListen(dynamic arguments, MockStreamHandlerEventSink events);
+
+  /// Handler for the cancel event
+  void onCancel(dynamic arguments);
+}
+
+/// A mock event sink for a [MockStreamHandler]
+class  MockStreamHandlerEventSink {
+  final EventSink<Object> _sink;
+
+  /// Constructor
+  MockStreamHandlerEventSink(this._sink);
+
+  /// Send a success event
+  void success(Object event) => _sink.add(event);
+
+  /// Send an error event
+  void error({
+    required String code,
+    String? message,
+    Object? details,
+  }) =>
+      _sink.addError(
+        PlatformException(code: code, message: message, details: details),
+      );
+
+  /// Send an end of stream event
+  void endOfStream() => _sink.close();
 }
--- test/mock_event_channel_test.dart
+++ test/mock_event_channel_test.dart
@@ -1,12 +1,33 @@
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:mock_event_channel/mock_event_channel.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('No arguments', (tester) async{
+    const channel = EventChannel('mock_event_channel');
+    channel.setMockStreamHandler(StreamHandler());
+
+    final stream = channel.receiveBroadcastStream();
+    stream.listen(print);
+    await expectLater(
+      stream,
+      emitsInOrder(
+        ['asdf', emitsError(PlatformException(code: 'asdf')), emitsDone],
+      ),
+    );
   });
+}
+
+class StreamHandler extends MockStreamHandler {
+  @override
+  void onListen(dynamic arguments, MockStreamHandlerEventSink events) {
+    events.success('asdf');
+    events.error(code: 'asdf');
+    events.endOfStream();
+  }
+
+  @override
+  void onCancel(dynamic arguments) {}
 }
